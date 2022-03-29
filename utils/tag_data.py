e46b0c224a541fb53e2638b0c41c65867ef1321a
Maciej Wojtowicz <maciek.wojtowicz97@gmail.com>
2022-03-29 21:18:22 +0000

    Tag data script refactor

--- utils/tag_data.py
+++ utils/tag_data.py
@@ -2,20 +2,20 @@ import influxdb
 import sys
 import pandas as pd
 
-headers_entropy = ['total_packets','tcp_packets','tcp_syn_packets','udp_packets','icmp_packets','avg_len','entropy_src_ip','entropy_src_port','label']
-headers_metric = ['total_packets','tcp_packets','tcp_syn_packets','udp_packets','icmp_packets','avg_len','label']
-headers_base = ['total_packets','avg_length','label']
-
-databases = {
-    "entropy" : ("ddos_entropy","ddos_e"),
-    "metrics" : ("ddos_metric_base","ddos_m"),
-    "normal"  : ("ddos_base","ddos_b")
+headers_entropy = ['total_packets',
+                   'tcp_packets',
+                   'tcp_syn_packets',
+                   'udp_packets',
+                   'icmp_packets',
+                   'avg_len',
+                   'entropy_src_ip',
+                   'entropy_src_port',
+                   'label']
+database = {
+    "entropy" : ("ddos_entropy","ddos_e")
 }
 
-QUERY_TIME_AGGREGATION = """select * from net order by time desc limit 1"""
 QUERY_ENTROPY = """select * from ddos_e order by time desc limit 300"""
-QUERY_METRICS = """select * from ddos_m order by time desc limit 500"""
-QUERY_BASE = """select count(length) as num_of_packet,mean(length) as size_of_data from net where time <= '' group by time(3s) order by time desc limit 10"""
 
 class MetricCollecter:
     def __init__(self, db_host = 'localhost', port = 8086, db = 'telegraf', measure_name = None, measurement_class = None, out_file = None, label = None):
@@ -27,10 +27,6 @@ class MetricCollecter:
         self.out_file = out_file
         self.label = label
         self.client = influxdb.InfluxDBClient(self.host,self.port, 'telegraf', 'telegraf', db)
-
-    def metric_base_aggregate_time(self):
-        measurement = self.client.query(QUERY_TIME_AGGREGATION).get_points(measurement = self.measure_name)
-        return measurement['time']
 
     def collect(self,q):
         dataframe = []
@@ -47,45 +43,11 @@ class MetricCollecter:
             data.append(self.label)
             dataframe.append(data)
         dataframe = pd.DataFrame(dataframe, columns = headers_entropy)
-        dataframe.to_csv(self.out_file, index = False)
+        dataframe.to_csv(self.out_file, index = False)    
 
-    def collect_metrics(self,q):
-        file = open(self.out_file,"w+")
-        for measurement in self.client.query(q).get_points(measurement = self.measure_name):
-            total_packets = measurement['total_packets']
-            tcp_packets = measurement['tcp_packets']
-            tcp_syn_packets = measurement['tcp_syn_packets']
-            udp_packets = measurement['udp_packets']
-            icmp_packets = measurement['icmp_packets']
-            avg_len = measurement['avg_len']
-            file.write("{}, {}, {}, {}, {}, {}\n".format(
-                total_packets,
-                tcp_packets,
-                tcp_syn_packets,
-                udp_packets,
-                icmp_packets,
-                avg_len,
-                self.label
-            ))
-        file.close()
-            
-    def collect_base(self,q):
-        file = open(self.out_file,"w+")
-        for measurement in self.client.query(q).get_points(measurement = self.measure_name):
-            cnt = measurement['num_of_packet']
-            mean_len = measurement['size_of_data']
-            file.write("{}, {}, {}\n".format(
-                cnt,
-                mean_len,
-                self.label
-            ))
-        file.close()
-        
+def actions(database_type, measure):
 
-
-def actions(database_type,measure):
-    global QUERY_BASE
-    db_name, measure_n = databases[database_type]
+    db_name, measure_n = database[database_type]
     collector = MetricCollecter(db = db_name,
                                 measure_name = measure_n,
                                 measurement_class = measure, 
@@ -94,31 +56,17 @@ def actions(database_type,measure):
 
     if database_type == "entropy":
         collector.collect(QUERY_ENTROPY)
-    elif database_type == "metrics":
-        collector.collect_metrics(QUERY_METRICS)
-    else:
-        timestamp = collector.metric_base_aggregate_time()
-        time = "'" + timestamp + "'"
-        QUERY_BASE = QUERY_BASE.replace("''",time)
-        collector.collect_base(QUERY_BASE)
         
 
 if __name__ == "__main__":
+    
     try:
-        database = sys.argv[1]
-    except IndexError as e:
-        print("Database to gather metrics not provided: {}".format(e))
-
-    try:
-        measurement_class = sys.argv[2] 
+        measurement_class = sys.argv[1] 
     except IndexError as e:
         print("Measurement class not provided: {}".format(e))
 
-    if database in databases:
-        actions(database,measurement_class)
-        print("Finished generating a label = {} training dataset!".format(measurement_class))
-    else:
-        print("Database {} doesn't exist".format(database))
+    actions('entropy', measurement_class)
+    print("Finished generating a label = {} training dataset!".format(measurement_class))
     
 
 
